4b11fe83f0468cd166d537655cc001915e098a68
Mohit_Matte <mattemohit2002@gmail.com>
2025-02-07 23:47:02 +0530

    added pipeline and data ingestion

--- src/constant/__init__.py
+++ src/constant/__init__.py
@@ -1,8 +1,8 @@
 import os
 
 AWS_S3_BUCKET_NAME=" "
-MONGO_DATABASE_NAME=" "
-MONGO_COLLECTION_NAME=" "
+MONGO_DATABASE_NAME="sensor_fault"
+MONGO_COLLECTION_NAME="sensor_fault_"
 
 TARGET_COLUMN=" "
 MONGO_DBL_URL=" "
@@ -10,3 +10,5 @@ MONGO_DBL_URL=" "
 
 MODEL_FILE_NAME=" "
 MODEL_FILE_EXTENSION=" "
+
+artifact_folder="artifacts"
